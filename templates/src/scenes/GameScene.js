54bded8eda0cef7effd8f7932cacb59ce0bd5dad
Ssteier2016 <109993596+Ssteier2016@users.noreply.github.com>
2025-04-27 14:20:17 -0300

    Update GameScene.js

--- templates/src/scenes/GameScene.js
+++ templates/src/scenes/GameScene.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import io from "socket.io-client";
+import VirtualJoystickPlugin from "@rexrainbow/phaser3-rex-plugins/plugins/virtualjoystick-plugin.js";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -8,12 +9,13 @@ export default class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.tilemapTiledJSON("palermo", "assets/maps/palermo.json");
-    this.load.image("tiles", "assets/tilesets/palermo.png");
-    this.load.spritesheet("player", "assets/sprites/player.png", { frameWidth: 32, frameHeight: 32 });
-    this.load.image("jaguar", "assets/sprites/jaguar.png");
-    this.load.image("chimera", "assets/sprites/chimera.png");
-    this.load.image("condor", "assets/sprites/condor.png");
+    this.load.tilemapTiledJSON("palermo", "../assets/maps/palermo.json");
+    this.load.image("tiles", "../assets/tilesets/palermo.png");
+    this.load.spritesheet("player", "../assets/sprites/player.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.image("jaguar", "../assets/sprites/jaguar.png");
+    this.load.image("chimera", "../assets/sprites/chimera.png");
+    this.load.image("condor", "../assets/sprites/condor.png");
+    this.load.plugin("rexvirtualjoystickplugin", VirtualJoystickPlugin, true);
   }
 
   create() {
@@ -28,8 +30,43 @@ export default class GameScene extends Phaser.Scene {
     this.cameras.main.startFollow(this.player, true, 0.1, 0.1);
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
-    this.cursors = this.input.keyboard.createCursorKeys();
+    // Animaciones
+    this.anims.create({
+      key: "walk-down",
+      frames: this.anims.generateFrameNumbers("player", { start: 0, end: 7 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "walk-up",
+      frames: this.anims.generateFrameNumbers("player", { start: 8, end: 15 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "walk-left",
+      frames: this.anims.generateFrameNumbers("player", { start: 16, end: 23 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "walk-right",
+      frames: this.anims.generateFrameNumbers("player", { start: 24, end: 31 }),
+      frameRate: 10,
+      repeat: -1,
+    });
 
+    // Controles
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.joystick = this.plugins.get("rexvirtualjoystickplugin").add(this, {
+      x: 100,
+      y: window.innerHeight - 100,
+      radius: 50,
+      base: this.add.circle(0, 0, 50, 0x888888),
+      thumb: this.add.circle(0, 0, 25, 0xcccccc),
+    });
+
+    // Socket.io
     this.socket = io();
     this.socket.on("connect", () => {
       this.socket.emit("newPlayer", { x: this.player.x, y: this.player.y });
@@ -48,12 +85,30 @@ export default class GameScene extends Phaser.Scene {
       });
     });
 
+    // Carta salvaje (Jaguar)
     const wildCard = this.physics.add.sprite(200, 200, "jaguar").setInteractive();
     this.physics.add.collider(wildCard, collisionLayer);
     wildCard.on("pointerdown", () => {
       this.scene.start("CombatScene", { opponent: { cardId: "#001", name: "Jaguar", attackLife: 50 } });
     });
 
+    // Carta suelta (Cóndor)
+    const collectibleCard = this.physics.add.sprite(250, 150, "condor").setInteractive();
+    this.physics.add.collider(collectibleCard, collisionLayer);
+    collectibleCard.on("pointerdown", () => {
+      this.socket.emit("collectCard", { cardId: "#003", name: "Cóndor", attackLife: 30 });
+      collectibleCard.destroy();
+    });
+
+    // Paquete (Quimera)
+    const cardPackage = this.physics.add.sprite(300, 300, "chimera").setInteractive();
+    this.physics.add.collider(cardPackage, collisionLayer);
+    cardPackage.on("pointerdown", () => {
+      this.socket.emit("collectCard", { cardId: "#002", name: "Quimera", attackLife: 70 });
+      cardPackage.destroy();
+    });
+
+    // Inventario
     this.input.keyboard.on("keydown-I", () => {
       this.scene.start("InventoryScene");
     });
@@ -63,15 +118,26 @@ export default class GameScene extends Phaser.Scene {
     const speed = 100;
     this.player.body.setVelocity(0);
 
-    if (this.cursors.left.isDown) {
+    let direction = null;
+    if (this.cursors.left.isDown || this.joystick.left) {
       this.player.body.setVelocityX(-speed);
-    } else if (this.cursors.right.isDown) {
+      direction = "walk-left";
+    } else if (this.cursors.right.isDown || this.joystick.right) {
       this.player.body.setVelocityX(speed);
+      direction = "walk-right";
     }
-    if (this.cursors.up.isDown) {
+    if (this.cursors.up.isDown || this.joystick.up) {
       this.player.body.setVelocityY(-speed);
-    } else if (this.cursors.down.isDown) {
+      direction = "walk-up";
+    } else if (this.cursors.down.isDown || this.joystick.down) {
       this.player.body.setVelocityY(speed);
+      direction = "walk-down";
+    }
+
+    if (direction) {
+      this.player.anims.play(direction, true);
+    } else {
+      this.player.anims.stop();
     }
 
     this.socket.emit("updatePosition", { x: this.player.x, y: this.player.y });
