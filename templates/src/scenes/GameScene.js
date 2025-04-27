0abdb55774b5253e88b5d02fdebd2a3a324eff4d
Ssteier2016 <rodrigo.n.arena@hotmail.com>
2025-04-27 14:10:04 -0300

    Agrega carpeta scenes y GameScene.js en templates/src

---
+++ templates/src/scenes/GameScene.js
@@ -0,0 +1,1 @@
+import Phaser from 'phaser';\n\nclass GameScene extends Phaser.Scene {\n  constructor() {\n    super('GameScene');\n  }\n  preload() {}\n  create() {}\n  update() {}\n}\nexport default GameScene;
