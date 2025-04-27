1ae4afa528b913aa244ff4a17489c7ad28f96360
Ssteier2016 <109993596+Ssteier2016@users.noreply.github.com>
2025-04-27 14:47:42 -0300

    Update Card.js

--- venv/modelos/Card.js
+++ venv/modelos/Card.js
@@ -1,12 +1,1 @@
-const mongoose = require("mongoose");
 
-const cardSchema = new mongoose.Schema({
-  cardId: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  attackLife: { type: Number, required: true },
-  experience: { type: Number, default: 0 },
-  level: { type: Number, default: 1 },
-  owner: { type: String }, // socket.id del jugador, null si es salvaje
-});
-
-module.exports = mongoose.model("Card", cardSchema);
