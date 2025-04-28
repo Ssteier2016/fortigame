2656f131252003d123b65b9fc16d92f27da9da62
Ssteier2016 <109993596+Ssteier2016@users.noreply.github.com>
2025-04-28 18:41:03 -0300

    Update and rename avatar to server-test.js

---
+++ venv/server-test.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Solicitud recibida en /");
+  res.send("¡Servidor de prueba funcionando!");
+});
+
+app.get("/test", (req, res) => {
+  console.log("Solicitud recibida en /test");
+  res.send("Servidor de prueba: Ruta /test");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor de prueba en http://localhost:${PORT}`);
+});
